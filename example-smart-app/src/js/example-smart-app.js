ae669afb2250da2befff4ca7adb62220e772e299
teckhuac <34991216+teckhuac@users.noreply.github.com>
2019-11-27 03:54:09 -0500

    Console object

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -132,7 +132,9 @@
     //   $('#risk').html(prediction);
     // });
 
-    window.location.href = "https://auxiliomd.pythonanywhere.com/risk_assessment_html?patientID=0&systolicbp=" + String(p.systolicbp)
+    console.log(p.systolicbp)
+
+    window.location.href = "https://auxiliomd.pythonanywhere.com/risk_assessment_html?patientID=0&systolicbp=" + p.systolicbp
   };
 
 })(window);
